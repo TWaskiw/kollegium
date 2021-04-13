9090974eef29f0121be14894e5dba4a601479980
Ellafroekjaer <71000446+Ellafroekjaer@users.noreply.github.com>
2021-04-13 21:43:50 +0200

    Api Madspild
    Fuld funktionsdygtig api til desktop

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-// #region madSpild api
+// #region madSpild
 
-var urll = "https://api.sallinggroup.com/v1/food-waste/?zip=8000"; //Api info fra Salling group
+var urll = "https://api.sallinggroup.com/v1/food-waste/?zip=8000";
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", urll);
 
-xhr.setRequestHeader("Authorization", "Bearer 43a92462-6aa7-4d98-8c48-402348293f73");//API nøglen, der bruges til at hente varer
-
+xhr.setRequestHeader("Authorization", "Bearer 43a92462-6aa7-4d98-8c48-402348293f73");
 let produkter = []
 xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
@@ -43,45 +42,18 @@ xhr.onreadystatechange = function () {
 
                 produkter.push(vare);
             }
-            let href = document.createElement('a');
-            let open = document.createElement('a');
-            let ul = document.querySelector('#butikker');
-            let linkText = document.createTextNode(butik[1] + ', ' + butik[2]);
-            let openText = document.createTextNode('(Åben mellem ' + grapClosing(butik[4]) + ' og ' + grapClosing(butik[3]) + ')')
 
-            if (a === 0) {
-                href.appendChild(linkText);
-                href.title = i;
-                href.classList.add("linkButik") // Tilføjer en Class til elementet, for at style fra CSS
-                //document.getElementById('butikker').appendChild(href); // Viser dem der ingen varer har.
-            } else {
-                href.appendChild(linkText);
-                href.title = i;
-                href.classList.add("linkButik") // Tilføjer en Class til elementet, for at style fra CSS
-
-                // document.getElementById('butikker').appendChild(href);
-                ul.appendChild(href);
-
-                open.classList.add("opening") // Tilføjer en Class til elementet, for at style fra CSS
-                ul.appendChild(openText)
-
-                // document.getElementById('butikker').appendChild(href);
-
+            if (a != '0') {
+                loadButikData(i, butik[1], butik[2], '(Åben mellem ' + formatTime(butik[4]) + ' og ' + formatTime(butik[3]) + ')')
             }
-        }
-        const linkButik = document.querySelectorAll(".linkButik");
-        for (const link of linkButik) {
 
+
+        }
+        const linkButik = document.querySelectorAll(".celle2");
+        for (const link of linkButik) {
             link.addEventListener('click', function () {
-                visVarer(this.title)
-                let n = this.innerHTML;
-                // let m = 
-                if (n.substring(0, 5) == 'Netto') {
-                    displayLogo('netto')
-                } else {
-                    displayLogo('foetex')
-                }
-                document.getElementById('varerBody').classList.add("change")
+                visVarer(this.id)
+
             })
         }
     }
@@ -89,59 +61,89 @@ xhr.onreadystatechange = function () {
 
 xhr.send();
 
+
+
 function visVarer(id) {
     let tbody = document.querySelector('#varerBody');
     tbody.innerHTML = "";
 
 
     for (v = 0; v < produkter.length; v++) {
-
-        console.log(produkter[v][0])
-
         if (produkter[v][0] == id) {
             loadTableData(produkter[v][1], produkter[v][2], produkter[v][3], produkter[v][4], produkter[v][5], produkter[v][6], produkter[v][7])
         }
     }
 }
 
+function getMeta(url) {
+    var img = new Image();
+    // console.log(img)
+    // console.log( img.width + ' ' + img.height );
+
+    img.addEventListener("load", function () {
+
+        // url = '/img/intet_billede.jpeg'
+        // console.log(url)
+
+
+        // console.log( img.width + ' ' + img.height );
+
+    });
+
+    img.src = url;
+    return url;
+}
+
+function loadButikData(id, navn, adresse, tid) {
+    const table = document.querySelector('.butik_wrapper')
+    let logo
+    if (navn === 'Netto') {
+        logo = 'img/netto_lille.png'
+    } else {
+        logo = 'img/foetex_lille.png'
+    }
+
+    let new_div = document.createElement("DIV");
+    new_div.innerHTML = "<div class='celle2' id=" + id + "><div class='liste_logo'><img class='celle2_billede' src='" + logo + "' alt='Logo'" + '' + "></img><p>" + adresse + "</p></div><div class='open'><p>" + tid + "</p></div></div>";
+
+    table.appendChild(new_div)
+};
+
 function loadTableData(navn, src, foerPris, nuPris, besparIP, besparIKR, beholdning) {
     let besparelse = Math.round(besparIP);
     const table = document.getElementById("varerBody");
-    console.log(src)
+    // console.log(src)
 
     let row = table.insertRow();
     let date = row.insertCell(0);
-    date.innerHTML = "<h3>" + navn + "</h3><p><br>Førpris: " + foerPris + " Kr<br>Tilbudspris: " + nuPris + " Kr<br>Besparelse: " + besparelse + "%<br>Antal: " + beholdning + "</p>";
+    if (src === null) {
 
-    let billede = row.insertCell(1);
-    let img = document.createElement("img");
-    img.src = src;
-    console.log(src)
-    if (src == null) {
-        billede.innerHTML = "<img src='/img/intet_billede.jpeg' alt='Intet billede' title='Intet billede'/>";
+        date.innerHTML = "<h3>" + navn + "</h3><div class='celle1'><div class='tpris'><img class='celle1_billede' src='img/intet_billede.jpeg' alt='Intet billede'" + '' + "></img><p>kr. <span>" + nuPris + ",-</span></p></div></div><div class='besparelse'><p>Besparelse</p>-" + besparelse + "%</div><div class='antal'><p>Antal</p>" + beholdning + " stk.</div>";
     } else {
-        billede.innerHTML = "<img src='" + src + "' alt='TEST'/>";
+        date.innerHTML = "<h3>" + navn + "</h3><div class='celle1'><div class='tpris'><img class='celle1_billede' src=" + getMeta(src) + "></img><p class='pris'>kr. <span>" + nuPris + ",-</span></p></div></div><div class='besparelse'><p>Besparelse</p>-" + besparelse + "%</div><div class='antal'><p>Antal</p>" + beholdning + " stk.</div>";
     }
 
 };
 
-function displayLogo(brand) { //Billeder der bruges til butikkerne
+function displayLogo(brand) {
     if (brand == 'netto') {
-        document.getElementById("logo").innerHTML = "<img src='./img/netto.png' alt='Netto billede'/>";
+        document.getElementById("logo").innerHTML = "<img src='img/netto_lille.png' alt='Netto billede'/>";
     } else {
-        document.getElementById("logo").innerHTML = "<img src='./img/foetex.png' alt='Netto billede'/>";
-    }//husk punktum før backslash for at loade billederne.
+        document.getElementById("logo").innerHTML = "<img src='img/foetex-lille.png' alt='Føtex billede'/>";
+    }
 }
 
 const capitalizeFirstLetter = (s) => {
     if (typeof s !== 'string') return ''
-    return s.charAt(0).toUpperCase() + s.slice(1) //Sørger for at butikkerne starter med stort bogstav
+    return s.charAt(0).toUpperCase() + s.slice(1)
 }
 
-const grapClosing = (s) => {
+const formatTime = (s) => {
     if (typeof s !== 'string') return ''
     let t = s.slice((s.length - 3) - 5)
 
     return t.slice(0, 5)
+
 }
-//#endregion madSpild api
+
+//#endregion madSpild
