62c663f273016c525d8f6ac6cb4c8ed8371e14fd
Ellafroekjaer <71000446+Ellafroekjaer@users.noreply.github.com>
2021-04-19 21:51:54 +0200

    Nu med login til iphone x + infobox

--- js/script.js
+++ js/script.js
@@ -144,10 +144,41 @@ const formatTime = (s) => {
     let t = s.slice((s.length - 3) - 5)
 
     return t.slice(0, 5)
-
 }
 
 //#endregion madSpild
+
+//#region Modal
+
+// Get the modal
+let modal = document.getElementById('infoModal')
+
+// Get the button that opens the modal
+let btn = document.getElementById('infoBtn')
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName('close')[0]
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+    modal.style.display = 'block'
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = 'none'
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = 'none'
+    }
+}
+
+//#endregion Modal
+
+
 
 // Google Autosearch API
 var searchInput = 'search_input';
@@ -165,3 +196,4 @@ $(document).ready(function () {
         var near_place = autocomplete.getPlace();
     });
 });
+
