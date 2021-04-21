1b16a4d60b8967f42a7ab79d461b975c7297ba1f
Sigurdmazanti <sigmn@live.dk>
2021-04-21 13:08:51 +0200

    js comment

--- js/script.js
+++ js/script.js
@@ -199,15 +199,17 @@ document.onclick = function (event) {
 // Google Autosearch API
 var searchInput = 'search_input';
 
+// Autocomplete funktionen
 $(document).ready(function () {
     var autocomplete;
     autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
+        // Begrænser funktionen til kun at virke i Danmark
         types: ['geocode'],
         componentRestrictions: {
             country: "DK"
         }
     });
-
+    // Fylder baren ud, når man skriver/trykker/vælger en adresse
     google.maps.event.addListener(autocomplete, 'place_changed', function () {
         var near_place = autocomplete.getPlace();
     });
